ddb1b5f18943844e66671e2a806d008a1670169f
Ibrahim Osama <55321754+Ibrahim-Osama@users.noreply.github.com>
2023-04-05 16:00:33 +0200

    Update my botr.py
    New features

--- my botr.py
+++ my botr.py
@@ -1,39 +1,56 @@
 import discord
-from discord.ext import commands
-import asyncio
+import random
+from discord.ext import commands, tasks
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print('Bot is ready')
+    print(f'Logged in as {bot.user.name}')
+    change_channels.start()
 
 @bot.command()
-async def move(ctx, channel: discord.VoiceChannel):
-    # Get the current voice channel of the member who sent the command
-    voice_channel = ctx.author.voice.channel
+async def ping(ctx):
+    await ctx.send('pong!')
 
-    # Move the member to the specified channel
-    await ctx.author.move_to(channel)
+@bot.command()
+async def move(ctx):
+    members = ctx.author.voice.channel.members
+    for member in members:
+        channel = random.choice(ctx.guild.voice_channels)
+        await member.move_to(channel)
 
-    # Wait for 10 seconds before moving the member back
-    await asyncio.sleep(10)
+@bot.command()
+async def back(ctx):
+    members = ctx.author.voice.channel.members
+    for member in members:
+        await member.move_to(ctx.author.voice.channel)
 
-    # Move the member back to the original channel
-    await ctx.author.move_to(voice_channel)
+@tasks.loop(count=4)
+async def change_channels():
+    guild = bot.get_guild(865951724249939969)
+    channels = guild.voice_channels
+    for channel in channels:
+        members = channel.members
+        if len(members) > 0:
+            new_channel = random.choice(channels)
+            while new_channel == channel:
+                new_channel = random.choice(channels)
+            for member in members:
+                await member.move_to(new_channel)
+    await discord.utils.sleep_until(change_channels.next_iteration)
+    members = guild.voice_channels[0].members
+    for member in members:
+        await member.move_to(guild.voice_channels[0])
+        
+@bot.command()
+async def helpme(ctx):
+    embed = discord.Embed(title="Bot Commands", description="Here are the bot commands you can use:", color=0x00ff00)
+    embed.add_field(name="!ping", value="Returns a 'pong!' message to check if the bot is working", inline=False)
+    embed.add_field(name="!move", value="Moves all members in your voice channel to a random voice channel in the server", inline=False)
+    embed.add_field(name="!back", value="Brings all members in your voice channel back to the original voice channel", inline=False)
+    embed.add_field(name="!helpme", value="Displays this help message", inline=False)
+    await ctx.send(embed=embed)
 
-    # Repeat the above steps three more times
-    for i in range(3):
-        # Wait for 10 seconds before moving the member to the next channel
-        await asyncio.sleep(10)
-
-        # Alternate between the original channel and the specified channel
-        if i % 2 == 0:
-            await ctx.author.move_to(channel)
-        else:
-            await ctx.author.move_to(voice_channel)
-
-    # Move the member back to the original channel
-    await ctx.author.move_to(voice_channel)
-
-bot.run('MTA5MTMwNTYyNjUyMjYyODEyNw.GsazXB.M9wBAQ4kdHfQjTpYXyYkthrk9vN66uPPAH-xAA')
+bot.run('MTA5MTMwNTYyNjUyMjYyODEyNw.GSTL6n.xhWdTAZuYy2fHyi8-SDmkaIDr5fn8-1AS7eNQ0')
